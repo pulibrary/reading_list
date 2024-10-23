bd647c317ba32a1d7ff9edfad7b6ebc27a1e3a40
Sean Warren <seanwarren77@gmail.com>
2024-10-23 14:44:01 -0400

    add page for new categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,7 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def new_categories
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   
   get "/categories", to:"categories#index"
 
+  get "/new_categories", to: "categories#new_categories"
+
+  resources :categories 
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
